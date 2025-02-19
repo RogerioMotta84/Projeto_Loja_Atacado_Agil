896271debe7b0f06ceb3faa8bc89ccf59b40153e
Rogério <rafa.roger1326@gmail.com>
2025-02-19 14:37:28 -0300

    Alteração no código e inclusão da pasta imagem

--- Loja Atacado Ágil X.py
+++ Loja Atacado Ágil X.py
@@ -1,22 +1,29 @@
 print('Bem vindo a loja Atacado Ágil X')
-valor_unitario = float(input('Entre com valor do produto:')) #valor unitario com valor do produto
-qtd_produto = int(input('Entre com a quantidade desejada:')) #quantidade de produtos
-valor_frete = 0 #frete
 
+# Recebe o valor do produto e substitui a vírgula por ponto
+valor_unitario = float(input('Entre com valor do produto:').replace(',', '.'))
+
+# Recebe a quantidade de produtos
+qtd_produto = int(input('Entre com a quantidade desejada:')) 
+
+# Inicializa o valor do frete
+valor_frete = 0
+
+# Condições para definir o valor do frete
 if 0 <= qtd_produto < 11:
-  valor_frete = 30
-
-elif 11<= qtd_produto < 101:
-  valor_frete = 60
-
-elif 101<= qtd_produto < 1001:
-  valor_frete = 120
-
+    valor_frete = 30
+elif 11 <= qtd_produto < 101:
+    valor_frete = 60
+elif 101 <= qtd_produto < 1001:
+    valor_frete = 120
 else:
-  valor_frete = 240
+    valor_frete = 240
 
-valor_parcial = float(valor_unitario * qtd_produto)
-valor_total = float(valor_parcial + float(valor_frete))
-print('O valor sem frete foi: R$ {:.2f}' . format(valor_parcial))
-print('O valor com frete foi: R$ {:.2f}' . format(valor_total) + ' (frete de R$ {:.2f}' . format(valor_frete) + ')')
+# Calcula o valor parcial e o valor total
+valor_parcial = valor_unitario * qtd_produto
+valor_total = valor_parcial + valor_frete
+
+# Exibe os resultados
+print('O valor sem frete foi: R$ {:.2f}'.format(valor_parcial))
+print('O valor com frete foi: R$ {:.2f} (frete de R$ {:.2f})'.format(valor_total, valor_frete))
 
